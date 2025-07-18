052dce43a2075f3d8bc8ce618b54cb48c53d0603
shrey312003 <shreybti@gmail.com>
2025-07-18 13:48:36 +0530

    Github host-1

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
+    basePath: '',
+    // assetPrefix: './',
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+  }
 
 
 export default nextConfig;
